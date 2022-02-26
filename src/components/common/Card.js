20dac5cc6650093af68454d74a66030eb5f68363
Chetan Waghade <Chetan.Waghade@godigit.com>
2022-02-26 13:16:25 +0530

    Added Few Components

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -5,6 +5,7 @@ const Card = () => {
   return (
     <div className="card_container">
       <p style={{ color: "#192a56" }}>Go to office</p>
+      <p>{new Date().toLocaleDateString()}</p>
     </div>
   );
 };
